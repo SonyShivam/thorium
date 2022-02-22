797c79c09f6014fa370cd2a5bc9a0bf7af2439f4
vassusony <vassusony1998@gmail.com>
2022-02-22 10:24:09 +0530

    New folder and filed added

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,10 @@
 const express = require('express');
-
+const logger = require('../logger/logger'); 
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
+    console.log(logger.welcome());
 });
 
 module.exports = router;
